f77a3a8ac2158565c8262bd70d52364aafd67e45
masudrahman-dev <masudrahman.dev@gmail>
2023-07-17 13:04:38 +0600

    modal component

--- weekly-tasks/week-1-and-2/src/App.tsx
+++ weekly-tasks/week-1-and-2/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import React, { useState } from 'react';
+import Modal from './components/Modal/Modal';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button onClick={() => setIsModalOpen(true)}>Open Modal with Text</button>
+      <button onClick={() => setIsModalOpen(false)}>Close Modal</button>
 
-export default App
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <h2>Hello, I am a Modal!</h2>
+        <p>This is an example of a reusable modal component.</p>
+      </Modal>
+    </div>
+  );
+};
+
+export default App;
